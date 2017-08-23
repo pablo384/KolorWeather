38de8d26f7af8fba372365d161983d242d1dfa09
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-23 12:49:58 -0400

    API Connection Created and Link

---
+++ app/src/main/java/com/pablo384/kolorweather/API/Connection.kt
@@ -0,0 +1,8 @@
+package com.pablo384.kolorweather.API
+
+/**
+ * Created by pablo384 on 23/08/17.
+ */
+
+val DARK_SKY_URL ="https://api.darksky.net/forecast/"
+val API_KEY ="a655ff78f6dffeda1202a1790322c87f"
--- app/src/main/java/com/pablo384/kolorweather/MainActivity.kt
+++ app/src/main/java/com/pablo384/kolorweather/MainActivity.kt
@@ -2,11 +2,20 @@ package com.pablo384.kolorweather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.pablo384.kolorweather.API.API_KEY
+import com.pablo384.kolorweather.API.DARK_SKY_URL
 
 class MainActivity : AppCompatActivity() {
+    val TAG= MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val latitud = 37.8267
+        val longitud =-122.4233
+
+        val url = "$DARK_SKY_URL$API_KEY/$latitud,$longitud"
+        Log.d(TAG,url)
     }
 }
