1b06b7efa049e3a62496202938023e5b57bc3827
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-23 17:54:48 -0400

    Better Code and Error fixed

--- app/src/main/java/com/pablo384/kolorweather/MainActivity.kt
+++ app/src/main/java/com/pablo384/kolorweather/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.pablo384.kolorweather
 
-import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
@@ -10,8 +10,6 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.pablo384.kolorweather.API.API_KEY
 import com.pablo384.kolorweather.API.DARK_SKY_URL
-import com.android.volley.VolleyError
-import com.android.volley.RequestQueue
 import com.pablo384.kolorweather.API.JSONParser
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
@@ -23,22 +21,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        precipTextView.text = getString(R.string.precip_placeholder,0)
+        tempTextView.text = getString(R.string.tem_placeholder, 0)
+        getWeather()
+    }
+
+    private fun getWeather() {
         val latitud = 37.8267
         val longitud = -122.4233
+        val language = getString(R.string.language)
+        val units = getString(R.string.units)
 
-        val url = "$DARK_SKY_URL$API_KEY/$latitud,$longitud?lang=es&units=si"
+        val url = "$DARK_SKY_URL$API_KEY/$latitud,$longitud?lang=$language&units=$units"
         Log.d(TAG, url)
         val queue = Volley.newRequestQueue(this)
         val stringRequest = StringRequest(Request.Method.GET, url,
                 Response.Listener { response ->
-                    with(JSONParser().getCurrentWeatherJSON(JSONObject(response))){
-                        descriptionTextView.text=summary
-                        precipTextView.text="${precip.toString()} %"
-                        tempTextView.text="${temp.toString()} C"
+                    with(JSONParser().getCurrentWeatherJSON(JSONObject(response))) {
+                        descriptionTextView.text = summary
+                        precipTextView.text = getString(R.string.precip_placeholder, (precip * 100).toInt())
+                        tempTextView.text = getString(R.string.tem_placeholder, temp.toInt())
                         iconImageView.setImageDrawable(resources.getDrawable(getIconResource()))
                     }
                 },
-                Response.ErrorListener { Log.d(TAG, "ERRROR Volley") })
+                Response.ErrorListener {displayErrorMessage()})
         queue.add(stringRequest)
+    }
+
+    private fun displayErrorMessage() {
+        val snackbar = Snackbar.make(main, "NETWORK ERROR", Snackbar.LENGTH_INDEFINITE)
+                .setAction("RETRY?", {
+                    getWeather()
+                })
+        snackbar.show()
     }
 }
