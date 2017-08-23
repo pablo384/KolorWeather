cb780547a57c45b3c7e88e74ca6965ad8b6b6e9e
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-23 13:57:54 -0400

    CurrentWeather Created and Volley Request

--- app/src/main/java/com/pablo384/kolorweather/MainActivity.kt
+++ app/src/main/java/com/pablo384/kolorweather/MainActivity.kt
@@ -3,19 +3,31 @@ package com.pablo384.kolorweather
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.pablo384.kolorweather.API.API_KEY
 import com.pablo384.kolorweather.API.DARK_SKY_URL
+import com.android.volley.VolleyError
+import com.android.volley.RequestQueue
+
 
 class MainActivity : AppCompatActivity() {
-    val TAG= MainActivity::class.java.simpleName
+    val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val latitud = 37.8267
-        val longitud =-122.4233
+        val longitud = -122.4233
 
         val url = "$DARK_SKY_URL$API_KEY/$latitud,$longitud"
-        Log.d(TAG,url)
+        Log.d(TAG, url)
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                Response.Listener { response -> Log.d(TAG, response.substring(0, 500))},
+                Response.ErrorListener { Log.d(TAG, "ERRROR Volley") })
+        queue.add(stringRequest)
     }
 }
---
+++ app/src/main/java/com/pablo384/kolorweather/models/CurrentWeather.kt
@@ -0,0 +1,25 @@
+package com.pablo384.kolorweather.models
+
+import com.pablo384.kolorweather.R
+
+/**
+ * Created by pablo384 on 23/08/17.
+ */
+class CurrentWeather(var icon:String, var summary:String, var temp:Double, var precip:Double){
+    fun getIconResource():Int{
+        when(icon){
+            "clear-night"-> return R.drawable.clear_night
+            "clear-day"-> return R.drawable.clear_day
+            "cloudy"-> return R.drawable.cloudy
+            "cloudy-night"-> return R.drawable.cloudy_night
+            "partly-cloudy"-> return R.drawable.partly_cloudy
+            "partly-cloudy-night"-> return R.drawable.partly_cloudy_night
+            "rain"-> return R.drawable.rain
+            "sleet"-> return R.drawable.sleet
+            "snow"-> return R.drawable.snow
+            "sunny"-> return R.drawable.sunny
+            "wind"-> return R.drawable.wind
+            else -> return R.drawable.na
+        }
+    }
+}
