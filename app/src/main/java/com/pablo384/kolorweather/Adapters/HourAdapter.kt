43ee721d2e03ac8dc8d6518e940f59c13e68a957
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-01 16:40:33 -0400

    Casi completa, falta probar

--- app/src/main/java/com/pablo384/kolorweather/Adapters/HourAdapter.kt
+++ app/src/main/java/com/pablo384/kolorweather/Adapters/HourAdapter.kt
@@ -1,48 +1,34 @@
 package com.pablo384.kolorweather.Adapters
 
-import android.os.Parcel
-import android.os.Parcelable
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.pablo384.kolorweather.R
+import com.pablo384.kolorweather.models.Hour
+import kotlinx.android.synthetic.main.hourly_item.view.*
 
 /**
  * Created by pablo384 on 01/09/17.
  */
-//class HourAdapter() : RecyclerView.Adapter<HourAdapter.ViewHolder>(), Parcelable {
-//    constructor(parcel: Parcel) : this() {
-//    }
-//
-//    override fun onBindViewHolder(holder: ?, position: Int) {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-//
-//    override fun getItemCount(): Int {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-//
-//    class ViewHolder {
-//
-//    }
-//
-//    override fun writeToParcel(parcel: Parcel, flags: Int) {
-//
-//    }
-//
-//    override fun describeContents(): Int {
-//        return 0
-//    }
-//
-//    companion object CREATOR : Parcelable.Creator<HourAdapter> {
-//        override fun createFromParcel(parcel: Parcel): HourAdapter {
-//            return HourAdapter(parcel)
-//        }
-//
-//        override fun newArray(size: Int): Array<HourAdapter?> {
-//            return arrayOfNulls(size)
-//        }
-//    }
-//}
+class HourAdapter(val daysList:ArrayList<Hour>) : RecyclerView.Adapter<HourAdapter.HourViewHolder>() {
+    override fun onBindViewHolder(holder: HourViewHolder, position: Int) {
+        return holder.bind(daysList[position])
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourViewHolder
+            =HourViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.hourly_item,parent,false))
+
+
+    override fun getItemCount(): Int= daysList.size
+
+
+    class HourViewHolder(hourlyItemView:View):RecyclerView.ViewHolder(hourlyItemView){
+        fun bind(hour:Hour)= with(itemView){
+            hourTextView.text=hour.getFormattedTime()
+            hourprecipTextView.text="${hour.precip.toInt()} %"
+            hourtempTextView.text="${hour.temp.toInt()} C"
+        }
+    }
+}
--- app/src/main/java/com/pablo384/kolorweather/UI/HourlyWeatherActivity.kt
+++ app/src/main/java/com/pablo384/kolorweather/UI/HourlyWeatherActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import com.pablo384.kolorweather.Adapters.HourAdapter
 import com.pablo384.kolorweather.R
 import com.pablo384.kolorweather.models.Hour
 import kotlinx.android.synthetic.main.activity_hourly_weather.*
@@ -16,10 +17,11 @@ class HourlyWeatherActivity : AppCompatActivity() {
         setContentView(R.layout.activity_hourly_weather)
         hourlyRecyclerView.setHasFixedSize(true)
         hourlyRecyclerView.layoutManager=LinearLayoutManager(this)
-//        hourlyRecyclerView.adapter=HourAdapter()
+
         intent.let {
             val hour:ArrayList<Hour> = it.getParcelableArrayListExtra(MainActivity.HOURLY_WEATHER)
             Log.d("TAG123", hour[0].toString())
+            hourlyRecyclerView.adapter= HourAdapter(hour)
         }
 
 
